9f3d2dc9879318067342408e3bd0d76b1acb3a44
waello <jlassiwael@gmail.com>
2024-01-29 03:07:41 +0100

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -53,11 +53,11 @@ class PalClass implements Pal {
   constructor(data: any) {
     this.name = data.name;
     this.paldeckNumber = data.id; // Assuming 'id' corresponds to 'paldeckNumber'
-    this.breedingPower = data.stats.find((stat: Stat) => stat.BreedPWR)?.BreedPWR || 0;
+    this.breedingPower = data.stats.find((stat: any) => stat.hasOwnProperty('BreedPWR'))?.BreedPWR || 0;
     this.type = Array.isArray(data.types) ?
         data.types.map((t: string) => t.charAt(0).toUpperCase() + t.slice(1).toLowerCase()) as PalType[] :
-        [];    this.skills = data.skills;
-    this.aura = data.aura;
+        [];
+    this.skills = data.skills;
     this.drops = data.drops;
     this.description = data.description;
     this.stats = this.mapStats(data.stats);
